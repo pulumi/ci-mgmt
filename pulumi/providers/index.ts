18dfa0951a519e0c3ccace6858016c2c58abb314
stack72 <public@paulstack.co.uk>
2020-09-22 12:07:06 +0100

    Change provider required status checks from Go 1.14 to Go 1.15

--- pulumi/providers/index.ts
+++ pulumi/providers/index.ts
@@ -18,14 +18,14 @@ for (let provider of providers) {
                 "prerequisites",
                 "lint-sdk",
                 "lint",
-                "test (3.1.301, 1.14.x, dotnet, 13.x, 3.7)",
-                "test (3.1.301, 1.14.x, go, 13.x, 3.7)",
-                "test (3.1.301, 1.14.x, nodejs, 13.x, 3.7)",
-                "test (3.1.301, 1.14.x, python, 13.x, 3.7)",
-                "build_sdk (3.1.301, 1.14.x, dotnet, 13.x, 3.7)",
-                "build_sdk (3.1.301, 1.14.x, go, 13.x, 3.7)",
-                "build_sdk (3.1.301, 1.14.x, nodejs, 13.x, 3.7)",
-                "build_sdk (3.1.301, 1.14.x, python, 13.x, 3.7)",
+                "test (3.1.301, 1.15.x, dotnet, 13.x, 3.7)",
+                "test (3.1.301, 1.15.x, go, 13.x, 3.7)",
+                "test (3.1.301, 1.15.x, nodejs, 13.x, 3.7)",
+                "test (3.1.301, 1.15.x, python, 13.x, 3.7)",
+                "build_sdk (3.1.301, 1.15.x, dotnet, 13.x, 3.7)",
+                "build_sdk (3.1.301, 1.15.x, go, 13.x, 3.7)",
+                "build_sdk (3.1.301, 1.15.x, nodejs, 13.x, 3.7)",
+                "build_sdk (3.1.301, 1.15.x, python, 13.x, 3.7)",
             ]
         }
     })
