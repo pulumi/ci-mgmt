0fccf5b4b5d7bef59447422a619248908a46f2f9
stack72 <public@paulstack.co.uk>
2021-06-07 21:48:17 +0100

    Adding the docs to the repo in prep for making it public

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# ci
-Configuration for all things CI
+# Pulumi CI Management
+
+This repository is a 1 stop shop for the enablement of CI / CD process for the Pulumi provider ecosystem. The list of providers
+this repository covers can be found in provider. The repository has 3 parts to it:
+
+* provider-ci
+* mp
+* infra
+
+## Provider CI
+
+This will generate github action workflow files for all Pulumi providers
+
+### Dependencies
+
+You'll need:
+
+- [jkcfg](https://github.com/jkcfg/jk/releases)
+- typescript
+- Make
+- npm
+
+### Building
+
+First, ensure the dependencies are installed:
+
+```
+$ npm install
+``` 
+
+Then, build the module:
+
+```
+$ make dist
+npx tsc
+```
+
+This will generate the module into `lib`
+
+### Config
+
+The configuration for each provider lives in `providers/<name>/config.yaml`.
+
+It takes  a few parameters, a `provider` string and a map, `env` with extra environment variables the provider needs. Here's an example:
+
+```
+provider: rancher2
+env:
+  RANCHER_INSECURE: true
+  AWS_REGION: us-west-2
+```
+
+There are also optional configuration values:
+
+```
+docker: true # whether the provider's test use docker to run, it expects a file `testing/docker-compose.yml`
+setup-script: testing/setup.sh # path to a script that's used for testing bootstraps
+```
+
+Once you have the configuration, you can generate a single provider like so:
+
+```
+make rancher2
+```
+
+Or, alternatively, generate all the providers in one go:
+
+```
+make providers
+```
+
+
+## MP
+
+Is where we deploy the actions changes above. We use https://github.com/Clever/microplane - currently v0.0.23 - for the deployment. To use it, you can run:
+
+```
+mp clone
+```
+
+This will clone the repositories to the mp folder. From there, we can run a plan of the changes across of the repositories
+
+```
+mp plan -b <branch name> -m "<commit message>" -- ~/code/go/src/github.com/pulumi/ci-mgmt/scripts/copy.sh ~/code/go/src/github.com/pulumi/ci-mgmt
+```
+
+we can then see a detail of the plan
+
+```
+mp status -r <repo-name>
+```
+
+When we are happy, we can deploy the changes to the repository
+
+```
+mp push -b <branch name> -t 180s
+```
+
+If we omit the `-t` command then we will flood the GitHub API and cause our actions to go into a waiting state
+
+## Infra
+
+This is a way of being able to manage the GitHub repository for each of the providers
---
+++ provider-ci/generate-provider.ts
@@ -0,0 +1,36 @@
+import * as shared from './lib/shared-workflows';
+import * as wf from './lib/workflows';
+import * as goreleaser from './lib/goreleaser';
+import * as lint from './lib/golangci';
+import * as param from '@jkcfg/std/param';
+
+const provider = param.String('provider');
+
+// eslint-disable-next-line no-template-curly-in-string
+const runAcceptanceTests = name => new wf.RunAcceptanceTestsWorkflow('run-acceptance-tests');
+const pullRequest = name => new wf.PullRequestWorkflow('pull-request');
+const master = name => new wf.MasterWorkflow('master');
+const preRelease = name => new wf.PrereleaseWorkflow('prerelease');
+const release = name => new wf.ReleaseWorkflow('release');
+const updatePulumiTerraformBridge = name => new wf.UpdatePulumiTerraformBridgeWorkflow();
+const commandDispatch = name => new wf.CommandDispatchWorkflow();
+const pre = name => new goreleaser.PulumiGoreleaserPreConfig(provider);
+const r = name => new goreleaser.PulumiGoreleaserConfig(provider);
+const lintConfig = name => new lint.PulumiGolangCIConfig();
+const cleanup = name => new shared.ArtifactCleanupWorkflow();
+const automation = name => new shared.AutoMergeWorkflow();
+
+export default [
+  { value: runAcceptanceTests('run-acceptance-tests'), file: `providers/${provider}/repo/.github/workflows/run-acceptance-tests.yml` },
+  { value: pullRequest('pull-request'), file: `providers/${provider}/repo/.github/workflows/pull-request.yml` },
+  { value: master('master'), file: `providers/${provider}/repo/.github/workflows/master.yml`},
+  { value: preRelease('prerelease'), file: `providers/${provider}/repo/.github/workflows/prerelease.yml` },
+  { value: release('release'), file: `providers/${provider}/repo/.github/workflows/release.yml` },
+  { value: automation('automation'), file: `providers/${provider}/repo/.github/workflows/pr-automation.yml` },
+  { value: cleanup('cleanup'), file: `providers/${provider}/repo/.github/workflows/artifact-cleanup.yml`},
+  { value: commandDispatch('command-dispatch'), file: `providers/${provider}/repo/.github/workflows/command-dispatch.yml`},
+  { value: updatePulumiTerraformBridge('update-pulumi-terraform-bridge'), file: `providers/${provider}/repo/.github/workflows/update-bridge.yml`},
+  { value: pre(provider), file: `providers/${provider}/repo/.goreleaser.prerelease.yml` },
+  { value: r(provider), file: `providers/${provider}/repo/.goreleaser.yml` },
+  { value: lintConfig('lint'), file: `providers/${provider}/repo/.golangci.yml` },
+];
