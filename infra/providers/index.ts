b11194e2ac04af722acb9b21ceca57a1e0ef2806
Paul Stack <public@paulstack.co.uk>
2022-01-26 17:56:57 +0000

    Rollout GHA for branch protection (#108)

--- infra/providers/index.ts
+++ infra/providers/index.ts
@@ -6,41 +6,47 @@ import * as yaml from 'yaml';
 
 
 // grab all the providers from the directory listing
-const providers = fs.readdirSync('../../actions/tf-providers/');
+const providers = fs.readdirSync('../../provider-ci/providers/');
 
 for (let provider of providers) {
-    let configFile = path.join("../../actions/tf-providers/", provider, "config.yaml")
+    let configFile = path.join("../../provider-ci/providers/", provider, "config.yaml")
     const providerConfig = yaml.parse(fs.readFileSync(configFile, 'utf8'))
 
     const contexts: string[] = [
         "Update Changelog",
-        "prerequisites (3.1.301, 1.16.x, 14.x, 3.7)",
-        "test (3.1.301, 1.16.x, dotnet, 14.x, 3.7)",
-        "test (3.1.301, 1.16.x, go, 14.x, 3.7)",
-        "test (3.1.301, 1.16.x, nodejs, 14.x, 3.7)",
-        "test (3.1.301, 1.16.x, python, 14.x, 3.7)",
-        "build_sdk (3.1.301, 1.16.x, dotnet, 14.x, 3.7)",
-        "build_sdk (3.1.301, 1.16.x, go, 14.x, 3.7)",
-        "build_sdk (3.1.301, 1.16.x, nodejs, 14.x, 3.7)",
-        "build_sdk (3.1.301, 1.16.x, python, 14.x, 3.7)",
+        "prerequisites (3.1.301, 1.17.x, 14.x, 3.7)",
+        "test (3.1.301, 1.17.x, dotnet, 14.x, 3.7)",
+        "test (3.1.301, 1.17.x, go, 14.x, 3.7)",
+        "test (3.1.301, 1.17.x, nodejs, 14.x, 3.7)",
+        "test (3.1.301, 1.17.x, python, 14.x, 3.7)",
+        "build_sdk (3.1.301, 1.17.x, dotnet, 14.x, 3.7)",
+        "build_sdk (3.1.301, 1.17.x, go, 14.x, 3.7)",
+        "build_sdk (3.1.301, 1.17.x, nodejs, 14.x, 3.7)",
+        "build_sdk (3.1.301, 1.17.x, python, 14.x, 3.7)",
     ];
 
     // If the lint step is null then we are using the default which is true
     // if we pass a lint value, it's more than likely to set it to false
     if (providerConfig.lint == null) {
-        contexts.push("lint (1.16.x)")
-        contexts.push("lint-sdk (1.16.x)")
+        contexts.push("lint (1.17.x)")
+        contexts.push("lint-sdk (1.17.x)")
     } else {
         console.log(`Skipping linting for ${provider}: ${providerConfig.lint}`)
     }
 
     // enable branchProtection
-    const branchProtection = new github.BranchProtection(`${provider}-branchprotection`, {
-        repositoryId: `pulumi-${provider}`,
-        pattern: 'master',
-        requiredStatusChecks: [{
-            strict: false,
-            contexts: contexts,
-        }]
-    })
+    const branches: string[] = [
+        "master",
+        "main"
+    ]
+    for (let branch of branches) {
+        const masterBranchProtection = new github.BranchProtection(`${provider}-${branch}-branchprotection`, {
+            repositoryId: `pulumi-${provider}`,
+            pattern: `${branch}`,
+            requiredStatusChecks: [{
+                strict: false,
+                contexts: contexts,
+            }]
+        })
+    }
 }
