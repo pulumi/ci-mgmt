a5af0b85b6e01317f9b02d2f6fb1ac6e355c41a8
Jamie Kinkead <jamie@pulumi.com>
2022-02-17 10:31:13 -0800

    Use single quotes for string literals

--- provider-ci/lib/workflows.js
+++ provider-ci/lib/workflows.js
@@ -36,7 +36,7 @@ export class DefaultBranchWorkflow extends g.GithubWorkflow {
                 branches: [name],
                 'tags-ignore': ['v*', 'sdk/*', '**'],
                 'paths-ignore': [
-                    "*.md"
+                    '*.md'
                 ]
             },
         }, {
