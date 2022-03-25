47447fecd88dba07113855892633ca7d8d776ed0
Daniel Bradley <daniel@pulumi.com>
2022-03-25 12:48:38 +0000

    Create new generate-providers script
    - Remove use of jk
    - Use inline version of jaxxstorm/gh-actions
    - Use zod to parse config yaml.
    - Generate and write yaml directly from typescript.
    - Use yargs for CLI options.
    - Allow generating all providers in single run.

---
+++ provider-ci/cmd/generate-providers.ts
@@ -0,0 +1,71 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as yaml from "yaml";
+import * as yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { buildProviderFiles, ProviderFile } from "../src/provider";
+
+export const providersDir = "providers";
+
+const args = yargs(hideBin(process.argv))
+  .command("generate-providers", "generate the providers")
+  .option("name", {
+    description: "Provider name to generate",
+    type: "string",
+    array: true,
+    alias: "n",
+  })
+  .option("debug", {
+    description: "Enable debug logging",
+    type: "boolean",
+  })
+  .parseSync();
+
+const debug = (message?: any, ...optionalParams: any[]) => {
+  if (args.debug) {
+    console.log(message, ...optionalParams);
+  }
+};
+
+const getProviders = (names: string[] | undefined) => {
+  const providers = fs
+    .readdirSync(providersDir, { withFileTypes: true })
+    .filter((dir) => dir.isDirectory)
+    .map((dir) => dir.name);
+
+  const providerSet = new Set(providers);
+  const unknown = names?.filter((n) => !providerSet.has(n)) ?? [];
+  if (unknown?.length > 0) {
+    throw new Error(`Unknown providers ${unknown?.join(", ")}`);
+  }
+
+  const nameSet = new Set(names);
+  if (names !== undefined) {
+    return providers.filter((n) => nameSet.has(n));
+  }
+  return providers;
+};
+
+interface Provider {
+  name: string;
+  files: ProviderFile[];
+}
+
+const writeProviderFiles = (provider: Provider) => {
+  const providerRepoPath = path.join(providersDir, provider.name, "repo");
+  for (const file of provider.files) {
+    const filePath = path.join(providerRepoPath, file.path);
+    const yamlContent = yaml.stringify(file.data, { sortMapEntries: true });
+    debug("Writing", filePath);
+    fs.writeFileSync(filePath, yamlContent, { encoding: "utf-8" });
+  }
+};
+
+const providerNames = getProviders(args.name);
+debug("providers to generate", providerNames);
+
+const providerFiles = providerNames.map((providerName) => ({
+  name: providerName,
+  files: buildProviderFiles(providerName),
+}));
+providerFiles.forEach(writeProviderFiles);
--- provider-ci/src/goreleaser.ts
+++ provider-ci/src/goreleaser.ts
@@ -1,191 +1,187 @@
-import * as param from '@jkcfg/std/param';
-
-const majVersion = param.Number('major-version', 2);
-const skipTfGen = param.Boolean('skipTfGen', false);
-const customLdFlag = param.String('customLdFlag') || "";
-const skipWindowsArmBuild = param.Boolean('skipWindowsArmBuild', false);
-
 export interface Build {
-    id?: string;
-    dir?: string;
-    main?: string;
-    binary?: string;
-    flags?: string[];
-    asmflags?: string[];
-    gcflags?: string[];
-    ldflags?: string[];
-    env?: string[];
-    goos?: string[];
-    goarch?: string[];
-    goarm?: string[];
-    skip?: boolean;
-    ignore?: Ignores[];
+  id?: string;
+  dir?: string;
+  main?: string;
+  binary?: string;
+  flags?: string[];
+  asmflags?: string[];
+  gcflags?: string[];
+  ldflags?: string[];
+  env?: string[];
+  goos?: string[];
+  goarch?: string[];
+  goarm?: string[];
+  skip?: boolean;
+  ignore?: Ignores[];
 }
 
 export interface FormatOverride {
-    goos?: string;
-    format?: string;
+  goos?: string;
+  format?: string;
 }
 export interface Archive {
-    id?: string;
-    name_template?: string;
-    builds?: string[];
-    wrap_in_directory?: boolean;
-    format?: string;
-    format_overrides?: FormatOverride[];
-    replacements?: {[k: string]: string};
+  id?: string;
+  name_template?: string;
+  builds?: string[];
+  wrap_in_directory?: boolean;
+  format?: string;
+  format_overrides?: FormatOverride[];
+  replacements?: { [k: string]: string };
 }
 
 export interface Ignores {
-    goos?: string;
-    goarch?: string;
+  goos?: string;
+  goarch?: string;
 }
 
 export interface Before {
-    hooks?: string[];
+  hooks?: string[];
 }
 
 export interface Snapshot {
-    name_template?: string;
+  name_template?: string;
 }
 
 export interface Changelog {
-    skip?: boolean;
-    use?: string;
-    sort?: string;
-    filters?: Filters,
+  skip?: boolean;
+  use?: string;
+  sort?: string;
+  filters?: Filters;
 }
 
 export interface Filters {
-    exclude?: string[];
+  exclude?: string[];
 }
 
 export interface Release {
-    disable?: boolean;
-    prerelease?: boolean;
+  disable?: boolean;
+  prerelease?: boolean;
 }
 
 export interface Blob {
-    provider?: string;
-    region?: string;
-    bucket?: string;
-    folder?: string;
-    ids?: string[];
+  provider?: string;
+  region?: string;
+  bucket?: string;
+  folder?: string;
+  ids?: string[];
 }
 
 export class GoreleaserConfig {
-    name: string;
-    builds: Build[];
-    archives: Archive[];
-    before: Before;
-    snapshot: Snapshot;
-    changelog: Changelog;
-    release: Release;
-    blobs: Blob[];
-    constructor(params?: Partial<GoreleaserConfig>) {
-        Object.assign(this, params)
-    }
+  name: string;
+  builds: Build[];
+  archives: Archive[];
+  before: Before;
+  snapshot: Snapshot;
+  changelog: Changelog;
+  release: Release;
+  blobs: Blob[];
+  constructor(params?: Partial<GoreleaserConfig>) {
+    Object.assign(this, params);
+  }
+}
+
+interface GoReleaserOpts {
+  skipWindowsArmBuild: boolean;
+  "major-version": number;
+  customLdFlag: string;
+  skipTfGen: boolean;
 }
 
 export class PulumiGoreleaserPreConfig extends GoreleaserConfig {
-    before: Before;
-    builds: Build[];
-    archives: Archive[];
-    snapshot: Snapshot;
-    changelog: Changelog;
-    release: Release;
-    blobs: Blob[];
+  before: Before;
+  builds: Build[];
+  archives: Archive[];
+  snapshot: Snapshot;
+  changelog: Changelog;
+  release: Release;
+  blobs: Blob[];
 
-    constructor(name: string) {
-        super();
+  constructor(name: string, opts: GoReleaserOpts) {
+    super();
 
-        let ldflags: string[];
-        let ignores: Ignores[] = [];
+    let ldflags: string[];
+    let ignores: Ignores[] = [];
 
-        if (skipWindowsArmBuild) {
-            ignores.push({goos: "windows", goarch:"arm64"})
-        }
-
-        if (majVersion! > 1 ) {
-            ldflags = [ `-X github.com/pulumi/pulumi-${name}/provider/v${majVersion}/pkg/version.Version={{.Tag}}` ]
-        } else {
-            ldflags = [ `-X github.com/pulumi/pulumi-${name}/provider/pkg/version.Version={{.Tag}}` ]
-        }
-
-        if (customLdFlag != "") {
-            ldflags.push(customLdFlag)
-        }
-
-        if (!skipTfGen) {
-            this.before = {
-                hooks: [
-                    'make tfgen'
-                ]
-            }
-        }
-        this.builds = [{
-            dir: 'provider',
-            env: [
-                'CGO_ENABLED=0',
-                'GO111MODULE=on'
-            ],
-            goos: [
-                'darwin',
-                'windows',
-                'linux',
-            ],
-            goarch: [
-                'amd64',
-                'arm64',
-            ],
-            ignore: ignores,
-            main: `./cmd/pulumi-resource-${name}/`,
-            ldflags: ldflags,
-            binary: `pulumi-resource-${name}`
-        }]
-        this.archives = [{
-            name_template: '{{ .Binary }}-{{ .Tag }}-{{ .Os }}-{{ .Arch }}',
-            id: 'archive',
-        }]
-        this.snapshot = {
-            name_template: '{{ .Tag }}-SNAPSHOT'
-        }
-        this.changelog = {
-            skip: true,
-        }
-        this.release = {
-            disable: true
-        }
-        this.blobs = [{
-            provider: 's3',
-            region: 'us-west-2',
-            bucket: 'get.pulumi.com',
-            folder: 'releases/plugins/',
-            ids: [ 'archive' ]
-        }]
-
-
+    if (opts.skipWindowsArmBuild) {
+      ignores.push({ goos: "windows", goarch: "arm64" });
     }
+
+    if (opts["major-version"]! > 1) {
+      ldflags = [
+        `-X github.com/pulumi/pulumi-${name}/provider/v${opts["major-version"]}/pkg/version.Version={{.Tag}}`,
+      ];
+    } else {
+      ldflags = [
+        `-X github.com/pulumi/pulumi-${name}/provider/pkg/version.Version={{.Tag}}`,
+      ];
+    }
+
+    if (opts.customLdFlag != "") {
+      ldflags.push(opts.customLdFlag);
+    }
+
+    if (!opts.skipTfGen) {
+      this.before = {
+        hooks: ["make tfgen"],
+      };
+    }
+    this.builds = [
+      {
+        dir: "provider",
+        env: ["CGO_ENABLED=0", "GO111MODULE=on"],
+        goos: ["darwin", "windows", "linux"],
+        goarch: ["amd64", "arm64"],
+        ignore: ignores,
+        main: `./cmd/pulumi-resource-${name}/`,
+        ldflags: ldflags,
+        binary: `pulumi-resource-${name}`,
+      },
+    ];
+    this.archives = [
+      {
+        name_template: "{{ .Binary }}-{{ .Tag }}-{{ .Os }}-{{ .Arch }}",
+        id: "archive",
+      },
+    ];
+    this.snapshot = {
+      name_template: "{{ .Tag }}-SNAPSHOT",
+    };
+    this.changelog = {
+      skip: true,
+    };
+    this.release = {
+      disable: true,
+    };
+    this.blobs = [
+      {
+        provider: "s3",
+        region: "us-west-2",
+        bucket: "get.pulumi.com",
+        folder: "releases/plugins/",
+        ids: ["archive"],
+      },
+    ];
+  }
 }
 
 export class PulumiGoreleaserConfig extends PulumiGoreleaserPreConfig {
-    constructor(name: string) {
-        super(name);
-        this.release = {
-            disable: false
-        }
-        this.changelog = {
-            use: 'git',
-            sort: 'asc',
-            filters: {
-                exclude: [
-                    "Merge branch",
-                    "Merge pull request",
-                    "\\Winternal\\W",
-                    "\\Wci\\W",
-                    "\\Wchore\\W",
-                ],
-            },
-        }
-    }
+  constructor(name: string, opts: GoReleaserOpts) {
+    super(name, opts);
+    this.release = {
+      disable: false,
+    };
+    this.changelog = {
+      use: "git",
+      sort: "asc",
+      filters: {
+        exclude: [
+          "Merge branch",
+          "Merge pull request",
+          "\\Winternal\\W",
+          "\\Wci\\W",
+          "\\Wchore\\W",
+        ],
+      },
+    };
+  }
 }
