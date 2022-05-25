b5db150e8c5dcd358b6a021ae87414fd591cb874
Jamie Kinkead <jamie@pulumi.com>
2022-05-25 13:59:08 -0700

    Remove unused code and only push command-dispatch where currently in use

--- native-provider-ci/src/provider.ts
+++ native-provider-ci/src/provider.ts
@@ -2,23 +2,15 @@ import * as fs from "fs";
 import * as path from "path";
 import * as yaml from "yaml";
 import z from "zod";
-import * as lint from "../../provider-ci/src/golangci";
-import * as goreleaser from "../../provider-ci/src/goreleaser";
 import * as shared from "./shared-workflows";
 import * as wf from "./workflows";
 import { providersDir } from "../cmd/generate-providers";
 
 const Config = z.object({
   provider: z.string(),
-  "upstream-provider-org": z.string(),
-  "upstream-provider-repo": z.string().optional(),
-  "fail-on-extra-mapping": z.boolean().default(true),
-  "fail-on-missing-mapping": z.boolean().default(true),
-  "upstream-provider-major-version": z.string().default(""),
   "provider-default-branch": z.string().default("master"),
   "golangci-timeout": z.string().default("20m"),
   "major-version": z.number().default(2),
-  skipTfGen: z.boolean().default(false),
   customLdFlag: z.string().default(""),
   skipWindowsArmBuild: z.boolean().default(false),
 });
@@ -29,11 +21,8 @@ const getProviderConfig = (provider: string) => {
   const parsed = z
     .intersection(Config, wf.WorkflowOpts)
     .parse(yaml.parse(content));
-  const upstreamProviderRepo =
-    parsed["upstream-provider-repo"] ?? `terraform-provider-${provider}`;
   return {
     ...parsed,
-    "upstream-provider-repo": upstreamProviderRepo,
   };
 };
 
