b035fccc37242cee7a96e532b1e525526ee8f6e6
stack72 <public@paulstack.co.uk>
2022-06-01 14:56:13 +0300

    Update branch protection checks to reference Go 1.18

--- infra/providers/index.ts
+++ infra/providers/index.ts
@@ -14,22 +14,22 @@ for (let provider of providers) {
 
     const contexts: string[] = [
         "Update Changelog",
-        "prerequisites (3.1.301, 1.17.x, 14.x, 3.7)",
-        "test (3.1.301, 1.17.x, dotnet, 14.x, 3.7)",
-        "test (3.1.301, 1.17.x, go, 14.x, 3.7)",
-        "test (3.1.301, 1.17.x, nodejs, 14.x, 3.7)",
-        "test (3.1.301, 1.17.x, python, 14.x, 3.7)",
-        "build_sdk (3.1.301, 1.17.x, dotnet, 14.x, 3.7)",
-        "build_sdk (3.1.301, 1.17.x, go, 14.x, 3.7)",
-        "build_sdk (3.1.301, 1.17.x, nodejs, 14.x, 3.7)",
-        "build_sdk (3.1.301, 1.17.x, python, 14.x, 3.7)",
+        "prerequisites (3.1.301, 1.18.x, 14.x, 3.7)",
+        "test (3.1.301, 1.18.x, dotnet, 14.x, 3.7)",
+        "test (3.1.301, 1.18.x, go, 14.x, 3.7)",
+        "test (3.1.301, 1.18.x, nodejs, 14.x, 3.7)",
+        "test (3.1.301, 1.18.x, python, 14.x, 3.7)",
+        "build_sdk (3.1.301, 1.18.x, dotnet, 14.x, 3.7)",
+        "build_sdk (3.1.301, 1.18.x, go, 14.x, 3.7)",
+        "build_sdk (3.1.301, 1.18.x, nodejs, 14.x, 3.7)",
+        "build_sdk (3.1.301, 1.18.x, python, 14.x, 3.7)",
     ];
 
     // If the lint step is null then we are using the default which is true
     // if we pass a lint value, it's more than likely to set it to false
     if (providerConfig.lint == null) {
-        contexts.push("lint (1.17.x)")
-        contexts.push("lint-sdk (1.17.x)")
+        contexts.push("lint (1.18.x)")
+        contexts.push("lint-sdk (1.18.x)")
     } else {
         console.log(`Skipping linting for ${provider}: ${providerConfig.lint}`)
     }
