5503096a948ee83e3af5ff0c847f81bba77b3a93
Paul Stack <public@paulstack.co.uk>
2022-07-14 19:50:54 +0100

    Upgrade pulumi-java-gen to enable autopublish (#219)

--- provider-ci/src/make-bridged-provider.ts
+++ provider-ci/src/make-bridged-provider.ts
@@ -12,7 +12,7 @@ export function bridgedProvider(config: BridgedConfig): Makefile {
   const VERSION_PATH = `$(PROVIDER_PATH)/pkg/version.Version`;
   const TFGEN = `pulumi-tfgen-$(PACK)`;
   const JAVA_GEN = `pulumi-java-gen`;
-  const JAVA_GEN_VERSION = "v0.4.1";
+  const JAVA_GEN_VERSION = "v0.5.0";
   const PROVIDER = `pulumi-resource-$(PACK)`;
   const VERSION = "$(shell pulumictl get version)";
   const TESTPARALLELISM = "10";
@@ -76,7 +76,7 @@ export function bridgedProvider(config: BridgedConfig): Makefile {
       "$(WORKING_DIR)/bin/$(TFGEN) nodejs --overlays provider/overlays/nodejs --out sdk/nodejs/",
       [
         "cd sdk/nodejs/",
-        'echo "module fake_nodejs_module // Exclude this directory from Go tools\\n\\ngo 1.16" > go.mod',
+        'echo "module fake_nodejs_module // Exclude this directory from Go tools\\n\\ngo 1.17" > go.mod',
         "yarn install",
         "yarn run tsc",
         "cp ../../README.md ../../LICENSE* package.json yarn.lock ./bin/",
@@ -94,7 +94,7 @@ export function bridgedProvider(config: BridgedConfig): Makefile {
       "$(WORKING_DIR)/bin/$(TFGEN) python --overlays provider/overlays/python --out sdk/python/",
       [
         "cd sdk/python/",
-        'echo "module fake_python_module // Exclude this directory from Go tools\\n\\ngo 1.16" > go.mod',
+        'echo "module fake_python_module // Exclude this directory from Go tools\\n\\ngo 1.17" > go.mod',
         "cp ../../README.md .",
         "python3 setup.py clean --all 2>/dev/null",
         "rm -rf ./bin/ ../python.bin/ && cp -R . ../python.bin && mv ../python.bin ./bin",
@@ -122,7 +122,7 @@ export function bridgedProvider(config: BridgedConfig): Makefile {
       "$(WORKING_DIR)/bin/$(TFGEN) dotnet --overlays provider/overlays/dotnet --out sdk/dotnet/",
       [
         "cd sdk/dotnet/",
-        'echo "module fake_dotnet_module // Exclude this directory from Go tools\\n\\ngo 1.16" > go.mod',
+        'echo "module fake_dotnet_module // Exclude this directory from Go tools\\n\\ngo 1.17" > go.mod',
         'echo "$(DOTNET_VERSION)" >version.txt',
         "dotnet build /p:Version=$(DOTNET_VERSION)",
       ],
@@ -142,10 +142,10 @@ export function bridgedProvider(config: BridgedConfig): Makefile {
       PACKAGE_VERSION: "$(shell pulumictl get version --language generic)",
     },
     commands: [
-        "$(WORKING_DIR)/bin/$(JAVA_GEN) generate --schema provider/cmd/$(PROVIDER)/schema.json --out sdk/java",
+        "$(WORKING_DIR)/bin/$(JAVA_GEN) generate --schema provider/cmd/$(PROVIDER)/schema.json --out sdk/java  --build gradle-nexus",
       [
         "cd sdk/java/",
-        'echo "module fake_java_module // Exclude this directory from Go tools\\n\\ngo 1.16" > go.mod',
+        'echo "module fake_java_module // Exclude this directory from Go tools\\n\\ngo 1.17" > go.mod',
         "gradle --console=plain build",
       ],
     ]
