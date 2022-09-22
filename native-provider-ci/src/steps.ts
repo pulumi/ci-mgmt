f1da6d7e27a8a1590f9cf980e16db9fe26f57a86
Levi Blackstone <levi@pulumi.com>
2022-09-22 12:41:22 -0600

    Fix weekly pulumi update for k8s provider

--- native-provider-ci/src/steps.ts
+++ native-provider-ci/src/steps.ts
@@ -873,7 +873,20 @@ export function CodegenDuringSDKBuild(provider: string) {
   return {};
 }
 
-export function UpdatePulumi(): Step {
+export function UpdatePulumi(provider: string): Step {
+  if (provider === "kubernetes") {
+    return {
+      name: "Update Pulumi/Pulumi",
+      id: "gomod",
+      run:
+        "git config --local user.email 'bot@pulumi.com'\n" +
+        "git config --local user.name 'pulumi-bot'\n" +
+        "git checkout -b update-pulumi/${{ github.run_id }}-${{ github.run_number }}\n" +
+        "make ensure\n" +
+        "git update-index -q --refresh\n" +
+        "if ! git diff-files --quiet; then \n\techo ::set-output name=changes::1 \nfi",
+    };
+  }
   return {
     name: "Update Pulumi/Pulumi",
     id: "gomod",
