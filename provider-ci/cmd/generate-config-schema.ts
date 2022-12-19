3d733813dcbcff2a88c3d81640029cf1cf605979
Ringo De Smet <ringo@de-smet.name>
2022-12-19 21:47:22 +0100

    Cleanup of migrated code from provider-ci
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

--- provider-ci/cmd/generate-config-schema.ts
+++
@@ -1,7 +0,0 @@
-import * as fs from "fs";
-import zodToJsonSchema from "zod-to-json-schema";
-import { Config } from "../src/config";
-
-const schema = zodToJsonSchema(Config, "ci-config");
-
-fs.writeFileSync("config-schema.json", JSON.stringify(schema, null, 2));
