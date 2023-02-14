6d168bab023a3409ca4a50af07d1979cedb62345
Anton Tayanovskyy <anton@pulumi.com>
2023-02-14 16:18:19 -0500

    Use retain on delete (#339)
    * Use retain on delete
    
    * Update Pulumi Node SDK
    
    * Undo #310

--- infra/providers/index.ts
+++ infra/providers/index.ts
@@ -9,22 +9,32 @@ const nativeProviders = fs.readdirSync("../../native-provider-ci/providers/")
 
 const providers = [...tfProviders, ...nativeProviders];
 
+function hasManagedBranchProtection(provider: string): boolean {
+    // Some, but not all of the providers under @pulumi/providers team have ad-hoc workflow names and do not want to
+    // manage branch protections in this stack. This list might grow as needed.
+    return !provider.includes("azure-native");
+}
+
 for (let provider of providers) {
     const contexts: string[] = [
         "Update Changelog",
-        "prerequisites",
-        "lint",
-        "lint-sdk",
-        "build_sdk (dotnet)",
-        "build_sdk (go)",
-        "build_sdk (java)",
-        "build_sdk (nodejs)",
-        "build_sdk (python)",
-        "test (dotnet)",
-        "test (go)",
-        "test (java)",
-        "test (nodejs)",
-        "test (python)",
+
+        // Undoing #310 for a bit so we can get retainOnDelete rolled out.
+        //
+        // "prerequisites",
+        // "lint",
+        // "lint-sdk",
+        // "build_sdk (dotnet)",
+        // "build_sdk (go)",
+        // "build_sdk (java)",
+        // "build_sdk (nodejs)",
+        // "build_sdk (python)",
+        // "test (dotnet)",
+        // "test (go)",
+        // "test (java)",
+        // "test (nodejs)",
+        // "test (python)",
+
         "sentinel",
     ];
 
@@ -34,13 +44,16 @@ for (let provider of providers) {
         "main"
     ]
     for (let branch of branches) {
-        const masterBranchProtection = new github.BranchProtection(`${provider}-${branch}-branchprotection`, {
+        new github.BranchProtection(`${provider}-${branch}-branchprotection`, {
             repositoryId: `pulumi-${provider}`,
             pattern: `${branch}`,
             requiredStatusChecks: [{
                 strict: false,
                 contexts: contexts,
             }]
+        }, {
+            // Prepare for no-op delete of azure-native and similar.
+            retainOnDelete: !hasManagedBranchProtection(provider),
         })
     }
 }
