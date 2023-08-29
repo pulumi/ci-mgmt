32292d82605e47ac4172cf6d6653548fc1eb341e
Matthew Jeffryes <mjeffryes@pulumi.com>
2023-08-29 16:33:21 -0700

    Create a daily workflow to update native-providers that are opted in to ci-mgmt

--- scripts/generate_native_providers_list.py
+++ scripts/generate_native_providers_list.py
@@ -1,4 +1,17 @@
+import argparse
 import os
 import json
 
-print(json.dumps(sorted(os.listdir('../native-provider-ci/providers'))))
+
+excluded_from_auto_pr =["azure-native", "google-native", "kubernetes", "command"]
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--for-auto-pr', help="only return the providers that should get an automatic PR", action="store_true")
+    args = ap.parse_args()
+
+    ps = sorted(os.listdir('../native-provider-ci/providers'))
+    if args.for_auto_pr:
+        ps = [p for p in ps if p not in excluded_from_auto_pr]
+
+    print(json.dumps(ps))
