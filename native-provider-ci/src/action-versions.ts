f7b8d1693b2eeb7655584db4daa6d36d6c85b0fa
Anton Tayanovskyy <anton@pulumi.com>
2023-09-13 18:10:19 -0400

    Update native providers

--- native-provider-ci/src/action-versions.ts
+++ native-provider-ci/src/action-versions.ts
@@ -40,5 +40,5 @@ export const uploadArtifact = "actions/upload-artifact@v3";
 export const githubScript = "actions/github-script@v6";
 export const upgradeProviderAction =
   "pulumi/pulumi-upgrade-provider-action@v0.0.5";
-export const publishProviderSDKs = "pulumi/pulumi-package-publisher@v0.0.7";
+export const publishProviderSDKs = "pulumi/pulumi-package-publisher@v0.0.11";
 export const slackNotification = "rtCamp/action-slack-notify@v2";
