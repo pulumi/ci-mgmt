5f05ecd8c08782104ed77b20369f65fa4b624b2b
Anton Tayanovskyy <anton@pulumi.com>
2023-09-15 18:40:47 -0400

    Wheels for bridged providers (#570)
    Experimenting here, will run through a few providers before rolling out
    to the rest.
    
    The Makefile edit has to go together with resources.go edit and
    regenerating Python files atomically in one PR this is a bit tricky.

--- tools/sourcemigrator/index.ts
+++ tools/sourcemigrator/index.ts
@@ -82,6 +82,27 @@ function updateGo_1_21(): SourceMigration {
     return sm;
 }
 
+function introducePythonWheels(): SourceMigration {
+    let edit = '&tfbridge.PythonInfo{x} -> (func () *tfbridge.PythonInfo { i := &tfbridge.PythonInfo{x}; i.PyProject.Enabled = true; return i })()';
+    let sm: SourceMigration = {
+        name: "introducePythonWheels",
+        execute: (ctx: MigrateContext) => {
+            let usesPyProjectTOML = fileContains(path.join(ctx.dir, "Makefile"),
+                                                 new RegExp("pyproject.toml"));
+            let alreadyApplied = fileContains(path.join(ctx.dir, "provider", "resources.go"),
+                                              new RegExp("PyProject.Enabled"));
+            if (alreadyApplied || !usesPyProjectTOML) {
+                return {filesEdited: 0};
+            };
+            child.execSync("gofmt -w -r '" + edit + "' provider/resources.go", {cwd: ctx.dir});
+            child.execSync("make tfgen build_python", {cwd: ctx.dir, stdio: 'inherit'});
+            child.execSync("rm -rf sdk/python/venv", {cwd: ctx.dir, stdio: 'inherit'});
+            return {filesEdited: 1};
+        },
+    };
+    return sm;
+}
+
 function fileContains(f: string, pattern: RegExp): boolean {
     let contents = String(fs.readFileSync(f));
     return pattern.test(contents);
@@ -118,6 +139,7 @@ function allMigrations(): SourceMigration[] {
         updateExamplesFromCore31DotNet6(),
         updatePulumiCoreRefTo3x(),
         updateGo_1_21(),
+        introducePythonWheels(),
     ];
 }
 
