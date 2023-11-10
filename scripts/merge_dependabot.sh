3909504b7229f188b2ad01aeeec8233a940ae2a7
Ian Wahbe <ian@wahbe.com>
2023-11-10 14:31:28 -0800

    Add bulk merge script dependabot

---
+++ scripts/merge_dependabot.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# This script will combine and set to merge all dependabot PRs in bridged provider repos.
+#
+# To guard against supply side attacks, this will only merge dependabot PRs that were
+# created before last week.
+
+# Ensure iwahbe/gh-combine-prs is installed
+#
+# --force: Ensure up to date, don't error when already installed.
+gh extension install iwahbe/gh-combine-prs --force
+
+# Query syntax is https://docs.github.com/en/search-github/searching-on-github/searching-issues-and-pull-requests.
+#
+# author:app/dependabot restricts this to PRs authored by dependabot.
+# created:<$(date -v-7d '+%Y-%m-%d') restricts to PRs created before 7 days ago
+QUERY="author:app/dependabot created:<$(date -v-7d '+%Y-%m-%d')"
+
+combine() {
+    provider="$1"
+    D="/tmp/pulumi-$provider"
+    rm -rf "$D"
+    (cd /tmp && git clone "https://github.com/pulumi/pulumi-$provider.git" "$D")
+    (cd "$D" && gh combine-prs \
+                   --skip-pr-check \
+                   --query "${QUERY}" \
+                   --auto-merge \
+                   --non-interactive \
+                   --labels impact/no-changelog-required)
+    rm -rf "$D"
+}
+
+if [[ "$#" -eq 1 ]]; then
+    # If the user has passed in a provider, update that provider.
+    combine "$1"
+else
+    # Otherwise update every provider.
+    jq -r '.[]' provider-ci/providers.json | while IFS= read -r provider; do
+        combine "$provider"
+    done
+fi
