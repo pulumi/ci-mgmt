061a21f1ede07ca21201d4935b16f9d825783707
Ramon Quitales <ramon@pulumi.com>
2023-12-08 11:59:55 -0800

    Update schema checking step to use pulumi/schema-tools in native providers (#739)

--- native-provider-ci/src/action-versions.ts
+++ native-provider-ci/src/action-versions.ts
@@ -18,7 +18,7 @@ export const googleAuth = "google-github-actions/auth@v0";
 export const goReleaser = "goreleaser/goreleaser-action@v2";
 export const gradleBuildAction =
   "gradle/gradle-build-action@9b814496b50909128c6a52622b416c5ffa04db49";
-export const installGhRelease = "jaxxstorm/action-install-gh-release@v1.5.0";
+export const installGhRelease = "jaxxstorm/action-install-gh-release@v1.10.0";
 export const installPulumiCli = "pulumi/actions@v4";
 export const codecov = "codecov/codecov-action@v3";
 
