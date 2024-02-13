900cbe3ae38a6e35f038cfa4d960167cb5fb7532
VenelinMartinov <venelin.v.martinov@gmail.com>
2024-02-13 17:38:05 +0000

    Update github actions (#818)
    Should mostly fix https://github.com/pulumi/ci-mgmt/issues/772 and
    silence some warnings.

--- native-provider-ci/src/action-versions.ts
+++ native-provider-ci/src/action-versions.ts
@@ -1,16 +1,16 @@
 // Languages
 export const goLint = "golangci/golangci-lint-action@v3";
-export const setupGo = "actions/setup-go@v3";
-export const setupDotNet = "actions/setup-dotnet@v3";
-export const setupJava = "actions/setup-java@v3";
+export const setupGo = "actions/setup-go@v5";
+export const setupDotNet = "actions/setup-dotnet@v4";
+export const setupJava = "actions/setup-java@v4";
 export const setupGradle = "gradle/gradle-build-action@v3";
-export const setupNode = "actions/setup-node@v2";
-export const setupPython = "actions/setup-python@v2";
+export const setupNode = "actions/setup-node@v4";
+export const setupPython = "actions/setup-python@v5";
 
 // Cloud Auth
 export const azureLogin = "azure/login@v1";
 export const configureAwsCredentials =
-  "aws-actions/configure-aws-credentials@v1";
+  "aws-actions/configure-aws-credentials@v4";
 export const setupGcloud = "google-github-actions/setup-gcloud@v0";
 export const googleAuth = "google-github-actions/auth@v0";
 
@@ -18,24 +18,24 @@ export const googleAuth = "google-github-actions/auth@v0";
 export const goReleaser = "goreleaser/goreleaser-action@v2";
 export const gradleBuildAction = "gradle/gradle-build-action@v3";
 export const installGhRelease = "jaxxstorm/action-install-gh-release@v1.10.0";
-export const installPulumiCli = "pulumi/actions@v4";
-export const codecov = "codecov/codecov-action@v3";
+export const installPulumiCli = "pulumi/actions@v5";
+export const codecov = "codecov/codecov-action@v4";
 
 // GHA Utilities
 export const addAndCommit = "EndBug/add-and-commit@v7";
 export const addLabel = "actions-ecosystem/action-add-labels@v1.1.0";
 export const autoMerge = "peter-evans/enable-pull-request-automerge@v1";
-export const checkout = "actions/checkout@v3";
+export const checkout = "actions/checkout@v4";
 export const cleanupArtifact = "c-hive/gha-remove-artifacts@v1";
 export const createOrUpdateComment = "peter-evans/create-or-update-comment@v1";
 export const deleteArtifact = "geekyeggo/delete-artifact@v1";
-export const downloadArtifact = "actions/download-artifact@v2";
+export const downloadArtifact = "actions/download-artifact@v4";
 export const notifySlack = "8398a7/action-slack@v3";
 export const pathsFilter = "dorny/paths-filter@v2";
 export const pullRequest = "repo-sync/pull-request@v2.6.2";
 export const prComment = "thollander/actions-comment-pull-request@v2";
 export const slashCommand = "peter-evans/slash-command-dispatch@v2";
-export const uploadArtifact = "actions/upload-artifact@v3";
+export const uploadArtifact = "actions/upload-artifact@v4";
 export const githubScript = "actions/github-script@v6";
 export const upgradeProviderAction =
   "pulumi/pulumi-upgrade-provider-action@v0.0.5";
