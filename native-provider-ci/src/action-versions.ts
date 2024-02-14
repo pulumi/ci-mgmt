pulumi/ci-mgmt

commit 900cbe3ae38a6e35f038cfa4d960167cb5fb7532

Update github actions (#818)
Should mostly fix https://github.com/pulumi/ci-mgmt/issues/772 and
silence some warnings.

native-provider-ci/src/action-versions.ts
@@ -1,16 +1,16 @@
 // Languages
 export const goLint = "golangci/golangci-lint-action@v3";
-export const setupGo = "actions/setup-go@v3";
-export const setupDotNet = "actions/setup-dotnet@v3";
-export const setupJava = "actions/setup-java@v3";
+export const setupGo = "actions/setup-go@v5";
+export const setupDotNet = "actions/setup-dotnet@v4";
+export const setupJava = "actions/setup-java@v4";
 export const setupGradle = "gradle/gradle-build-action@v3";
-export const setupNode = "actions/setup-node@v2";
-export const setupPython = "actions/setup-python@v2";
+export const setupNode = "actions/setup-node@v4";
+export const setupPython = "actions/setup-python@v5";
 
 // Cloud Auth
 export const azureLogin = "azure/login@v1";
 export const configureAwsCredentials =
-  "aws-actions/configure-aws-credentials@v1";
+  "aws-actions/configure-aws-credentials@v4";
 export const setupGcloud = "google-github-actions/setup-gcloud@v0";
 export const googleAuth = "google-github-actions/auth@v0";
 
@@ -18,24 +18,24 @@ export const googleAuth = "google-github-actions/auth@v0";
 export const goReleaser = "goreleaser/goreleaser-action@v2";
 export const gradleBuildAction = "gradle/gradle-build-action@v3";
 export const installGhRelease = "jaxxstorm/action-install-gh-release@v1.10.0";
-export const installPulumiCli = "pulumi/actions@v4";
-export const codecov = "codecov/codecov-action@v3";
+export const installPulumiCli = "pulumi/actions@v5";
+export const codecov = "codecov/codecov-action@v4";
 
 // GHA Utilities
 export const addAndCommit = "EndBug/add-and-commit@v7";
 export const addLabel = "actions-ecosystem/action-add-labels@v1.1.0";
 export const autoMerge = "peter-evans/enable-pull-request-automerge@v1";
-export const checkout = "actions/checkout@v3";
+export const checkout = "actions/checkout@v4";
 export const cleanupArtifact = "c-hive/gha-remove-artifacts@v1";
 export const createOrUpdateComment = "peter-evans/create-or-update-comment@v1";
 export const deleteArtifact = "geekyeggo/delete-artifact@v1";
-export const downloadArtifact = "actions/download-artifact@v2";
+export const downloadArtifact = "actions/download-artifact@v4";
 export const notifySlack = "8398a7/action-slack@v3";
 export const pathsFilter = "dorny/paths-filter@v2";
 export const pullRequest = "repo-sync/pull-request@v2.6.2";
 export const prComment = "thollander/actions-comment-pull-request@v2";
 export const slashCommand = "peter-evans/slash-command-dispatch@v2";
-export const uploadArtifact = "actions/upload-artifact@v3";
+export const uploadArtifact = "actions/upload-artifact@v4";
 export const githubScript = "actions/github-script@v6";
 export const upgradeProviderAction =
   "pulumi/pulumi-upgrade-provider-action@v0.0.5";

provider-ci/internal/pkg/templates/bridged-provider.config.yaml
@@ -40,12 +40,12 @@ env:
 actions:
   setupPulumi:
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: "^3"
   setupGo:
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         go-version: 1.21.x
         cache-dependency-path: |
@@ -67,13 +67,13 @@ runner:
   # publish: ubuntu-latest
   # buildSdk: ubuntu-latest
 actionVersions:
-  setupDotNet: actions/setup-dotnet@v3
-  setupJava: actions/setup-java@v3
+  setupDotNet: actions/setup-dotnet@v4
+  setupJava: actions/setup-java@v4
   setupGradle: gradle/gradle-build-action@v3
-  setupNode: actions/setup-node@v2
-  setupPython: actions/setup-python@v2
+  setupNode: actions/setup-node@v4
+  setupPython: actions/setup-python@v5
 
-  configureAwsCredentials: aws-actions/configure-aws-credentials@v1
+  configureAwsCredentials: aws-actions/configure-aws-credentials@v4
   setupGcloud: google-github-actions/setup-gcloud@v0
   googleAuth: google-github-actions/auth@v2
   goReleaser: goreleaser/goreleaser-action@v2
@@ -82,12 +82,12 @@ actionVersions:
   pulumictlTag: v0.0.46
   cleanupArtifact: c-hive/gha-remove-artifacts@v1
   createOrUpdateComment: peter-evans/create-or-update-comment@v1
-  downloadArtifact: actions/download-artifact@v2
+  downloadArtifact: actions/download-artifact@v4
   notifySlack: 8398a7/action-slack@v3
   pathsFilter: dorny/paths-filter@v2
   prComment: thollander/actions-comment-pull-request@v2
   slashCommand: peter-evans/slash-command-dispatch@v2
-  uploadArtifact: actions/upload-artifact@v2
+  uploadArtifact: actions/upload-artifact@v4
   upgradeProviderAction: pulumi/pulumi-upgrade-provider-action@v0.0.11
   slackNotification: rtCamp/action-slack-notify@v2
 publish:

provider-ci/test-workflows/aws/.github/workflows/master.yml
@@ -49,7 +49,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -60,24 +60,24 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - name: Setup Node
-      uses: actions/setup-node@v2
+      uses: actions/setup-node@v4
       with:
         node-version: ${{ env.NODEVERSION }}
         registry-url: https://registry.npmjs.org
     - name: Setup DotNet
-      uses: actions/setup-dotnet@v3
+      uses: actions/setup-dotnet@v4
       with:
         dotnet-version: ${{ env.DOTNETVERSION }}
     - name: Setup Python
-      uses: actions/setup-python@v2
+      uses: actions/setup-python@v5
       with:
         python-version: ${{ env.PYTHONVERSION }}
     - name: Setup Java
-      uses: actions/setup-java@v3
+      uses: actions/setup-java@v4
       with:
         cache: gradle
         distribution: temurin
@@ -87,7 +87,7 @@ jobs:
       with:
         gradle-version: ${{ env.GRADLEVERSION }}
     - name: Download provider + tfgen binaries
-      uses: actions/download-artifact@v2
+      uses: actions/download-artifact@v4
       with:
         name: ${{ env.PROVIDER }}-provider.tar.gz
         path: ${{ github.workspace }}/bin
@@ -111,7 +111,7 @@ jobs:
     - name: Compress SDK folder
       run: tar -zcf sdk/${{ matrix.language }}.tar.gz -C sdk/${{ matrix.language }} .
     - name: Upload artifacts
-      uses: actions/upload-artifact@v2
+      uses: actions/upload-artifact@v4
       with:
         name: ${{ matrix.language  }}-sdk.tar.gz
         path: ${{ github.workspace}}/sdk/${{ matrix.language }}.tar.gz
@@ -145,7 +145,7 @@ jobs:
       with:
         submodules: true
     - name: Configure AWS Credentials
-      uses: aws-actions/configure-aws-credentials@v1
+      uses: aws-actions/configure-aws-credentials@v4
       with:
         aws-access-key-id: ${{ secrets.AWS_CORP_S3_UPLOAD_ACCESS_KEY_ID }}
         aws-region: us-west-2
@@ -159,7 +159,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -170,7 +170,7 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - if: github.event_name == 'pull_request'
@@ -212,7 +212,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -223,7 +223,7 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - if: github.event_name == 'pull_request'
@@ -273,7 +273,7 @@ jobs:
         github.workspace }}/bin/ pulumi-resource-${{ env.PROVIDER }}
         pulumi-tfgen-${{ env.PROVIDER }}
     - name: Upload artifacts
-      uses: actions/upload-artifact@v2
+      uses: actions/upload-artifact@v4
       with:
         name: ${{ env.PROVIDER }}-provider.tar.gz
         path: ${{ github.workspace }}/bin/provider.tar.gz
@@ -302,7 +302,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -313,11 +313,11 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - name: Configure AWS Credentials
-      uses: aws-actions/configure-aws-credentials@v1
+      uses: aws-actions/configure-aws-credentials@v4
       with:
         aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
         aws-region: us-east-2
@@ -400,7 +400,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -411,24 +411,24 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - name: Setup Node
-      uses: actions/setup-node@v2
+      uses: actions/setup-node@v4
       with:
         node-version: ${{ env.NODEVERSION }}
         registry-url: https://registry.npmjs.org
     - name: Setup DotNet
-      uses: actions/setup-dotnet@v3
+      uses: actions/setup-dotnet@v4
       with:
         dotnet-version: ${{ env.DOTNETVERSION }}
     - name: Setup Python
-      uses: actions/setup-python@v2
+      uses: actions/setup-python@v5
       with:
         python-version: ${{ env.PYTHONVERSION }}
     - name: Setup Java
-      uses: actions/setup-java@v3
+      uses: actions/setup-java@v4
       with:
         cache: gradle
         distribution: temurin
@@ -438,7 +438,7 @@ jobs:
       with:
         gradle-version: ${{ env.GRADLEVERSION }}
     - name: Download provider + tfgen binaries
-      uses: actions/download-artifact@v2
+      uses: actions/download-artifact@v4
       with:
         name: ${{ env.PROVIDER }}-provider.tar.gz
         path: ${{ github.workspace }}/bin
@@ -450,7 +450,7 @@ jobs:
         find ${{ github.workspace }} -name "pulumi-*-${{ env.PROVIDER }}" -print -exec chmod +x {} \;
     - run: dotnet nuget add source ${{ github.workspace }}/nuget
     - name: Download SDK
-      uses: actions/download-artifact@v2
+      uses: actions/download-artifact@v4
       with:
         name: ${{ matrix.language }}-sdk.tar.gz
         path: ${{ github.workspace}}/sdk/
@@ -471,7 +471,7 @@ jobs:
         token: ${{ secrets.GITHUB_TOKEN }}
         version: v2.4.0
     - name: Configure AWS Credentials
-      uses: aws-actions/configure-aws-credentials@v1
+      uses: aws-actions/configure-aws-credentials@v4
       with:
         aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
         aws-region: ${{ env.AWS_REGION }}
@@ -511,7 +511,7 @@ jobs:
               submodules: true
           - name: Make upstream
             run: make upstream
-          - uses: actions/setup-go@v4
+          - uses: actions/setup-go@v5
             with:
               cache-dependency-path: provider/go.sum
               go-version-file: provider/go.mod
@@ -522,7 +522,7 @@ jobs:
           - env:
               CODECOV_TOKEN: ${{ secrets.CODECOV_TOKEN }}
             name: Upload coverage reports to Codecov
-            uses: codecov/codecov-action@v3
+            uses: codecov/codecov-action@v4
       timeout-minutes: 60
 
 name: master

provider-ci/test-workflows/aws/.github/workflows/prerelease.yml
@@ -50,7 +50,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -61,24 +61,24 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - name: Setup Node
-      uses: actions/setup-node@v2
+      uses: actions/setup-node@v4
       with:
         node-version: ${{ env.NODEVERSION }}
         registry-url: https://registry.npmjs.org
     - name: Setup DotNet
-      uses: actions/setup-dotnet@v3
+      uses: actions/setup-dotnet@v4
       with:
         dotnet-version: ${{ env.DOTNETVERSION }}
     - name: Setup Python
-      uses: actions/setup-python@v2
+      uses: actions/setup-python@v5
       with:
         python-version: ${{ env.PYTHONVERSION }}
     - name: Setup Java
-      uses: actions/setup-java@v3
+      uses: actions/setup-java@v4
       with:
         cache: gradle
         distribution: temurin
@@ -88,7 +88,7 @@ jobs:
       with:
         gradle-version: ${{ env.GRADLEVERSION }}
     - name: Download provider + tfgen binaries
-      uses: actions/download-artifact@v2
+      uses: actions/download-artifact@v4
       with:
         name: ${{ env.PROVIDER }}-provider.tar.gz
         path: ${{ github.workspace }}/bin
@@ -112,7 +112,7 @@ jobs:
     - name: Compress SDK folder
       run: tar -zcf sdk/${{ matrix.language }}.tar.gz -C sdk/${{ matrix.language }} .
     - name: Upload artifacts
-      uses: actions/upload-artifact@v2
+      uses: actions/upload-artifact@v4
       with:
         name: ${{ matrix.language  }}-sdk.tar.gz
         path: ${{ github.workspace}}/sdk/${{ matrix.language }}.tar.gz
@@ -153,7 +153,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -164,7 +164,7 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - if: github.event_name == 'pull_request'
@@ -214,7 +214,7 @@ jobs:
         github.workspace }}/bin/ pulumi-resource-${{ env.PROVIDER }}
         pulumi-tfgen-${{ env.PROVIDER }}
     - name: Upload artifacts
-      uses: actions/upload-artifact@v2
+      uses: actions/upload-artifact@v4
       with:
         name: ${{ env.PROVIDER }}-provider.tar.gz
         path: ${{ github.workspace }}/bin/provider.tar.gz
@@ -243,7 +243,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -254,11 +254,11 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - name: Configure AWS Credentials
-      uses: aws-actions/configure-aws-credentials@v1
+      uses: aws-actions/configure-aws-credentials@v4
       with:
         aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
         aws-region: us-east-2
@@ -323,7 +323,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -334,24 +334,24 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - name: Setup Node
-      uses: actions/setup-node@v2
+      uses: actions/setup-node@v4
       with:
         node-version: ${{ env.NODEVERSION }}
         registry-url: https://registry.npmjs.org
     - name: Setup DotNet
-      uses: actions/setup-dotnet@v3
+      uses: actions/setup-dotnet@v4
       with:
         dotnet-version: ${{ env.DOTNETVERSION }}
     - name: Setup Python
-      uses: actions/setup-python@v2
+      uses: actions/setup-python@v5
       with:
         python-version: ${{ env.PYTHONVERSION }}
     - name: Setup Java
-      uses: actions/setup-java@v3
+      uses: actions/setup-java@v4
       with:
         cache: gradle
         distribution: temurin
@@ -361,7 +361,7 @@ jobs:
       with:
         gradle-version: ${{ env.GRADLEVERSION }}
     - name: Download provider + tfgen binaries
-      uses: actions/download-artifact@v2
+      uses: actions/download-artifact@v4
       with:
         name: ${{ env.PROVIDER }}-provider.tar.gz
         path: ${{ github.workspace }}/bin
@@ -373,7 +373,7 @@ jobs:
         find ${{ github.workspace }} -name "pulumi-*-${{ env.PROVIDER }}" -print -exec chmod +x {} \;
     - run: dotnet nuget add source ${{ github.workspace }}/nuget
     - name: Download SDK
-      uses: actions/download-artifact@v2
+      uses: actions/download-artifact@v4
       with:
         name: ${{ matrix.language }}-sdk.tar.gz
         path: ${{ github.workspace}}/sdk/
@@ -394,7 +394,7 @@ jobs:
         token: ${{ secrets.GITHUB_TOKEN }}
         version: v2.4.0
     - name: Configure AWS Credentials
-      uses: aws-actions/configure-aws-credentials@v1
+      uses: aws-actions/configure-aws-credentials@v4
       with:
         aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
         aws-region: ${{ env.AWS_REGION }}
@@ -434,7 +434,7 @@ jobs:
               submodules: true
           - name: Make upstream
             run: make upstream
-          - uses: actions/setup-go@v4
+          - uses: actions/setup-go@v5
             with:
               cache-dependency-path: provider/go.sum
               go-version-file: provider/go.mod
@@ -445,7 +445,7 @@ jobs:
           - env:
               CODECOV_TOKEN: ${{ secrets.CODECOV_TOKEN }}
             name: Upload coverage reports to Codecov
-            uses: codecov/codecov-action@v3
+            uses: codecov/codecov-action@v4
       timeout-minutes: 60
 
 name: prerelease

provider-ci/test-workflows/aws/.github/workflows/release.yml
@@ -49,7 +49,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -60,24 +60,24 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - name: Setup Node
-      uses: actions/setup-node@v2
+      uses: actions/setup-node@v4
       with:
         node-version: ${{ env.NODEVERSION }}
         registry-url: https://registry.npmjs.org
     - name: Setup DotNet
-      uses: actions/setup-dotnet@v3
+      uses: actions/setup-dotnet@v4
       with:
         dotnet-version: ${{ env.DOTNETVERSION }}
     - name: Setup Python
-      uses: actions/setup-python@v2
+      uses: actions/setup-python@v5
       with:
         python-version: ${{ env.PYTHONVERSION }}
     - name: Setup Java
-      uses: actions/setup-java@v3
+      uses: actions/setup-java@v4
       with:
         cache: gradle
         distribution: temurin
@@ -87,7 +87,7 @@ jobs:
       with:
         gradle-version: ${{ env.GRADLEVERSION }}
     - name: Download provider + tfgen binaries
-      uses: actions/download-artifact@v2
+      uses: actions/download-artifact@v4
       with:
         name: ${{ env.PROVIDER }}-provider.tar.gz
         path: ${{ github.workspace }}/bin
@@ -111,7 +111,7 @@ jobs:
     - name: Compress SDK folder
       run: tar -zcf sdk/${{ matrix.language }}.tar.gz -C sdk/${{ matrix.language }} .
     - name: Upload artifacts
-      uses: actions/upload-artifact@v2
+      uses: actions/upload-artifact@v4
       with:
         name: ${{ matrix.language  }}-sdk.tar.gz
         path: ${{ github.workspace}}/sdk/${{ matrix.language }}.tar.gz
@@ -167,7 +167,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -178,7 +178,7 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - if: github.event_name == 'pull_request'
@@ -228,7 +228,7 @@ jobs:
         github.workspace }}/bin/ pulumi-resource-${{ env.PROVIDER }}
         pulumi-tfgen-${{ env.PROVIDER }}
     - name: Upload artifacts
-      uses: actions/upload-artifact@v2
+      uses: actions/upload-artifact@v4
       with:
         name: ${{ env.PROVIDER }}-provider.tar.gz
         path: ${{ github.workspace }}/bin/provider.tar.gz
@@ -257,7 +257,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -268,11 +268,11 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - name: Configure AWS Credentials
-      uses: aws-actions/configure-aws-credentials@v1
+      uses: aws-actions/configure-aws-credentials@v4
       with:
         aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
         aws-region: us-east-2
@@ -340,7 +340,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - name: Checkout Repo
-      uses: actions/checkout@v3
+      uses: actions/checkout@v4
     - name: Unshallow clone for PR merge bases
       run: git fetch --prune --unshallow --tags
     - name: Clean up release labels
@@ -373,7 +373,7 @@ jobs:
     - name: Unshallow clone for tags
       run: git fetch --prune --unshallow --tags
     - name: Install Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5
       with:
         cache-dependency-path: |
             sdk/go.sum
@@ -384,24 +384,24 @@ jobs:
         tag: v0.0.46
         repo: pulumi/pulumictl
     - name: Install Pulumi CLI
-      uses: pulumi/actions@v4
+      uses: pulumi/actions@v5
       with:
         pulumi-version: ^3
     - name: Setup Node
-      uses: actions/setup-node@v2
+      uses: actions/setup-node@v4
       with:
         node-version: ${{ env.NODEVERSION }}
         registry-url: https://registry.npmjs.org
     - name: Setup DotNet
-      uses: actions/setup-dotnet@v3
+      uses: actions/setup-dotnet@v4
       with:
         dotnet-version: ${{ env.DOTNETVERSION }}
     - name: Setup Python
-      uses: actions/setup-python@v2
+      uses: actions/setup-python@v5
       with:
         python-version: ${{ env.PYTHONVERSION }}
     - name: Setup Java
-      uses: actions/setup-java@v3
+      uses: actions/setup-java@v4
       with:
         cache: gradle
         distribution: temurin
@@ -411,7 +411,7 @@ jobs:
       with:
         gradle-version: ${{ env.GRADLEVERSION }}
     - name: Download provider + tfgen binaries
-      uses: actions/download-artifact@v2
+      uses: actions/download-artifact@v4
       with:
         name: ${{ env.PROVIDER }}-provider.tar.gz
         path: ${{ github.workspace }}/bin
@@ -423,7 +423,7 @@ jobs:
         find ${{ github.workspace }} -name "pulumi-*-${{ env.PROVIDER }}" -print -exec chmod +x {} \;
     - run: dotnet nuget add source ${{ github.workspace }}/nuget
     - name: Download SDK
-      uses: actions/download-artifact@v2
+      uses: actions/download-artifact@v4
       with:
         name: ${{ matrix.language }}-sdk.tar.gz
         path: ${{ github.workspace}}/sdk/
@@ -444,7 +444,7 @@ jobs:
         token: ${{ secrets.GITHUB_TOKEN }}
         version: v2.4.0
     - name: Configure AWS Credentials
-      uses: aws-actions/configure-aws-credentials@v1
+      uses: aws-actions/configure-aws-credentials@v4
       with:
         aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
         aws-region: ${{ env.AWS_REGION }}
@@ -484,7 +484,7 @@ jobs:
               submodules: true
           - name: Make upstream
             run: make upstream
-          - uses: actions/setup-go@v4
+          - uses: actions/setup-go@v5
             with:
               cache-dependency-path: provider/go.sum
               go-version-file: provider/go.mod
@@ -495,7 +495,7 @@ jobs:
           - env:
               CODECOV_TOKEN: ${{ secrets.CODECOV_TOKEN }}
             name: Upload coverage reports to Codecov
-            uses: codecov/codecov-action@v3
+            uses: codecov/codecov-action@v4
       timeout-minutes: 60
 
 name: release

commit 6397366577bfd6843dd8ddbd980a7e039aedbf82

Consume pulumi-package-publisher@v0.0.14 (#822)
Upgrades to
https://github.com/pulumi/pulumi-package-publisher/releases/tag/v0.0.14,
fixing the problem introduced in #818.

native-provider-ci/src/action-versions.ts
@@ -39,5 +39,4 @@ export const uploadArtifact = "actions/upload-artifact@v4";
 export const githubScript = "actions/github-script@v6";
 export const upgradeProviderAction =
   "pulumi/pulumi-upgrade-provider-action@v0.0.5";
-export const publishProviderSDKs = "pulumi/pulumi-package-publisher@v0.0.7";
 export const slackNotification = "rtCamp/action-slack-notify@v2";

provider-ci/internal/pkg/templates/bridged-provider.config.yaml
@@ -91,5 +91,5 @@ actionVersions:
   upgradeProviderAction: pulumi/pulumi-upgrade-provider-action@v0.0.11
   slackNotification: rtCamp/action-slack-notify@v2
 publish:
-  publisherAction: pulumi/pulumi-package-publisher@v0.0.13
+  publisherAction: pulumi/pulumi-package-publisher@v0.0.14
   sdk: all

provider-ci/test-workflows/aws/.github/workflows/master.yml
@@ -347,7 +347,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - name: Publish SDKs
-      uses: pulumi/pulumi-package-publisher@v0.0.13
+      uses: pulumi/pulumi-package-publisher@v0.0.14
       with:
         sdk: all
     - env:

provider-ci/test-workflows/aws/.github/workflows/prerelease.yml
@@ -288,7 +288,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - name: Publish SDKs
-      uses: pulumi/pulumi-package-publisher@v0.0.13
+      uses: pulumi/pulumi-package-publisher@v0.0.14
       with:
         sdk: all
     - env:

provider-ci/test-workflows/aws/.github/workflows/release.yml
@@ -301,7 +301,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - name: Publish SDKs
-      uses: pulumi/pulumi-package-publisher@v0.0.13
+      uses: pulumi/pulumi-package-publisher@v0.0.14
       with:
         sdk: all
     - env:

provider-ci/test-workflows/cloudflare/.github/workflows/master.yml
@@ -333,7 +333,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - name: Publish SDKs
-      uses: pulumi/pulumi-package-publisher@v0.0.13
+      uses: pulumi/pulumi-package-publisher@v0.0.14
       with:
         sdk: all
     - env:

provider-ci/test-workflows/cloudflare/.github/workflows/prerelease.yml
@@ -276,7 +276,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - name: Publish SDKs
-      uses: pulumi/pulumi-package-publisher@v0.0.13
+      uses: pulumi/pulumi-package-publisher@v0.0.14
       with:
         sdk: all
     - env:

provider-ci/test-workflows/cloudflare/.github/workflows/release.yml
@@ -289,7 +289,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - name: Publish SDKs
-      uses: pulumi/pulumi-package-publisher@v0.0.13
+      uses: pulumi/pulumi-package-publisher@v0.0.14
       with:
         sdk: all
     - env:

provider-ci/test-workflows/docker/.github/workflows/master.yml
@@ -346,7 +346,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - name: Publish SDKs
-      uses: pulumi/pulumi-package-publisher@v0.0.13
+      uses: pulumi/pulumi-package-publisher@v0.0.14
       with:
         sdk: all
     - env:

provider-ci/test-workflows/docker/.github/workflows/prerelease.yml
@@ -289,7 +289,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - name: Publish SDKs
-      uses: pulumi/pulumi-package-publisher@v0.0.13
+      uses: pulumi/pulumi-package-publisher@v0.0.14
       with:
         sdk: all
     - env:

provider-ci/test-workflows/docker/.github/workflows/release.yml
@@ -302,7 +302,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - name: Publish SDKs
-      uses: pulumi/pulumi-package-publisher@v0.0.13
+      uses: pulumi/pulumi-package-publisher@v0.0.14
       with:
         sdk: all
     - env: