e25edd8060a56df62936ac2f807404e34058de52
Daniel Bradley <daniel@pulumi.com>
2025-02-05 11:01:40 +0000

    Add "internal-bridged" template
    This is files which are only for bridged providers, owned by Pulumi. This was requested to not be included for external providers because they configure upgrades more manually.
    
    This removes the empty file being generated for "generic" providers and allows us to remove the NoUpstream conditional hack.

--- provider-ci/internal/pkg/templates.go
+++ provider-ci/internal/pkg/templates.go
@@ -18,6 +18,8 @@ const (
 	external TemplateDir = "external"
 	// Makefile for bridged providers (internal & external)
 	bridged TemplateDir = "bridged"
+	// Upgrade config
+	internalBridged TemplateDir = "internal-bridged"
 )
 
 // getTemplateDirs returns a list of directories in the embedded filesystem that form the overall template.
@@ -30,7 +32,7 @@ func getTemplateDirs(templateName string) ([]TemplateDir, error) {
 	switch templateName {
 	case "bridged-provider":
 		// Any Pulumi-owned bridged provider
-		return []TemplateDir{base, internal, bridged}, nil
+		return []TemplateDir{base, internal, bridged, internalBridged}, nil
 	case "external-bridged-provider":
 		// third-party bridged providers
 		return []TemplateDir{base, external, bridged}, nil
