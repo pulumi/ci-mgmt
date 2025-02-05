4d0a821bfe779e167650937d5be25d87471962c1
Daniel Bradley <daniel@pulumi.com>
2025-02-05 19:30:21 +0000

    Simplify internal template naming & organisation (#1360)
    Each provider template is a combination of:
    1. `base` - files common to all providers (this contains the majority of
    all templates)
    2. `internal` or `external` - which add customisations for providers
    owned by Pulumi or providers owned by 3rd parties.
    3. `bridged` - for bridged providers–not generic providers such as eks
    or pulumiservice
    4. `internal-bridged` - adds a single special customisation for
    non-third party bridged providers.
    
    This has no impact on the generated files except for adding the missing
    devContainer support for "generic" providers.
    
    I'd recommend use the separated commits for reviewing each logical step.
    
    ## Steps
    
    1. Combine "devContainer" and "provider" into "base"
       - This adds the devContainer support to "generic" providers.
    
    2. Flatten "provider-ci" into "base"
    - "provider-ci" is use in all templates so can just live in base to
    avoid adding additional splitting of folders.
    
    3. Rename "pulumi-provider" to "internal"
    -This pairs better with "external" which is used for third party
    providers.
    
    4. Rename "external-provider" to "external"
    - Everything's a provider within "provider-ci" so no need for the
    extension.
    
    5. Rename "bridged-provider" to "bridged"
       - Drop the "provider" suffix.
    
    6. Add "internal-bridged" template
    - This is files which are only for bridged providers, owned by Pulumi.
    This was requested to not be included for external providers because
    they configure upgrades more manually.
    - This removes the empty file being generated for "generic" providers
    and allows us to remove the NoUpstream conditional hack.
    
    7. Move Makefile to "base"
    - This is currently used for all templates, not only bridged providers.
    
    8. Remove "bridged" folder from "generic"
    - The generic template no longer renders any of the files from the
    bridged template as they're all wrapped in conditionals which are never
    true for generic provider configurations.
    - This allows us to remove the NoUpstream conditionals from the two
    files in the bridged template.
    
    9. Remove empty .upgrade.config.yml for generic providers
    - This was intended to be excluded by a global conditional round the
    template, but was actually generated as an empty file instead which was
    not the desired outcome.

--- provider-ci/internal/pkg/templates.go
+++ provider-ci/internal/pkg/templates.go
@@ -10,18 +10,16 @@ type TemplateDir string
 
 // Template directories
 const (
-	// configuration to run the repository in a dev container
-	devContainer TemplateDir = "dev-container"
-	// git attributes, go config, CI reusable workflows
-	provider TemplateDir = "provider"
+	// git attributes, go config, CI reusable workflows, dev container, generic CI
+	base TemplateDir = "base"
 	// CoC, upgrade-provider config, issue templates, command dispatch workflows
-	pulumiProvider TemplateDir = "pulumi-provider"
+	internal TemplateDir = "internal"
 	// ci-mgmt pull-based updates workflow
-	externalProvider TemplateDir = "external-provider"
-	// Generic provider CI templates
-	providerCi = "provider-ci"
+	external TemplateDir = "external"
 	// Makefile for bridged providers (internal & external)
-	bridgedProvider TemplateDir = "bridged-provider"
+	bridged TemplateDir = "bridged"
+	// Upgrade config
+	internalBridged TemplateDir = "internal-bridged"
 )
 
 // getTemplateDirs returns a list of directories in the embedded filesystem that form the overall template.
@@ -34,13 +32,13 @@ func getTemplateDirs(templateName string) ([]TemplateDir, error) {
 	switch templateName {
 	case "bridged-provider":
 		// Any Pulumi-owned bridged provider
-		return []TemplateDir{devContainer, provider, pulumiProvider, providerCi, bridgedProvider}, nil
+		return []TemplateDir{base, internal, bridged, internalBridged}, nil
 	case "external-bridged-provider":
 		// third-party bridged providers
-		return []TemplateDir{devContainer, provider, externalProvider, providerCi, bridgedProvider}, nil
+		return []TemplateDir{base, external, bridged}, nil
 	case "generic":
-		// currently almost identical to the bridged-provider template
-		return []TemplateDir{provider, pulumiProvider, providerCi, bridgedProvider}, nil
+		// Pulumi-owned providers not based on tf-bridge
+		return []TemplateDir{base, internal}, nil
 	default:
 		return nil, fmt.Errorf("unknown template: %s", templateName)
 	}
