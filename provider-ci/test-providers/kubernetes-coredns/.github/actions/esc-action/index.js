a973aef7b0742e3e21cbb4ed330e869d71b98789
Bryce Lampe <bryce@pulumi.com>
2025-08-20 14:54:21 -0700

    regenerate testdata

--- provider-ci/test-providers/kubernetes-coredns/.github/actions/esc-action/index.js
+++ provider-ci/test-providers/kubernetes-coredns/.github/actions/esc-action/index.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 
-for (const [name, value] of Object.entries(process.env)) {
-  const file = process.env["GITHUB_OUTPUT"];
-  var stream = fs.createWriteStream(file, { flags: "a" });
+const file = process.env["GITHUB_OUTPUT"];
+var stream = fs.createWriteStream(file, { flags: "a" });
 
+for (const [name, value] of Object.entries(process.env)) {
   try {
     stream.write(`${name}=${value}\n`);
   } catch (err) {
     console.log(`error: failed to set output for ${name}: ${err.message}`);
   }
-
-  stream.end();
 }
+
+stream.end();
