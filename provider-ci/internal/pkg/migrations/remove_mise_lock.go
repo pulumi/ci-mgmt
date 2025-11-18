ac216265621b9f224cd9c8261a400c58a39a8a31
Bryce Lampe <bryce@pulumi.com>
2025-11-18 11:21:05 -0800

    lint

--- provider-ci/internal/pkg/migrations/remove_mise_lock.go
+++ provider-ci/internal/pkg/migrations/remove_mise_lock.go
@@ -3,9 +3,7 @@ package migrations
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"strings"
 )
 
 // Remove mise.lock file if present - we no longer generate lockfiles
@@ -33,46 +31,4 @@ func (maintainMiseLock) Migrate(templateName, outDir string) error {
 	// If the file doesn't exist, nothing to do
 
 	return nil
-}
-
-func runMiseCommand(pulumiVersion, goVersion, outDir string, args ...string) error {
-	cmd := exec.Command("mise", args...)
-	cmd.Dir = outDir
-	cmd.Env = append(os.Environ(),
-		fmt.Sprintf("PULUMI_VERSION_MISE=%s", pulumiVersion),
-		fmt.Sprintf("GO_VERSION_MISE=%s", goVersion),
-	)
-	output, err := cmd.CombinedOutput()
-	fmt.Println(string(output))
-	if err != nil {
-		return fmt.Errorf("error running mise install: %w\nOutput: %s", err, string(output))
-	}
-	return nil
-}
-
-func getVersions(outdir string) (string, string, error) {
-	cmd := exec.Command("scripts/get-versions.sh")
-	cmd.Dir = filepath.Join(outdir)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return "", "", err
-	}
-	res := strings.Split(string(output), "\n")
-
-	var goVersion string
-	var pulumiVersion string
-	for _, val := range res {
-		key, val, ok := strings.Cut(val, "=")
-		if ok && key == "PULUMI_VERSION_MISE" {
-			pulumiVersion = val
-		}
-		if ok && key == "GO_VERSION_MISE" {
-			goVersion = val
-		}
-	}
-	if goVersion == "" || pulumiVersion == "" {
-		return "", "", fmt.Errorf("error getting versions: pulumiVersion=%s; goVersion=%s", pulumiVersion, goVersion)
-	}
-
-	return pulumiVersion, goVersion, nil
 }
