b42d55e39feb86e3d70e5ce68c6d23c8259cf681
Bryce Lampe <bryce@pulumi.com>
2025-12-03 13:07:12 -0800

    debugging

--- provider-ci/internal/pkg/migrations/activate_mise.go
+++ provider-ci/internal/pkg/migrations/activate_mise.go
@@ -3,6 +3,7 @@ package migrations
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"os"
 	"os/exec"
 )
@@ -26,17 +27,19 @@ func (activateMise) Migrate(_ string, outDir string) error {
 	cmd.Stdout = buf
 
 	if err := cmd.Run(); err != nil {
-		return err
+		return fmt.Errorf("running mise: %w", err)
 	}
 
 	var values map[string]string
 
 	if err := json.Unmarshal(buf.Bytes(), &values); err != nil {
-		return err
+		return fmt.Errorf("parsing mise output: %w", err)
 	}
 
 	for name, value := range values {
-		_ = os.Setenv(name, value)
+		if err := os.Setenv(name, value); err != nil {
+			return fmt.Errorf("setting mise env: %w", err)
+		}
 	}
 
 	return nil
